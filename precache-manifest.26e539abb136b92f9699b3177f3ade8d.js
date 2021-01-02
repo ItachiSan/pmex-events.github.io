fe3fd52e137edbdecaee8d8ddb8d7ee6a30dc0e1
Giovanni Santini <giovannisantini93@yahoo.it>
2021-01-02 03:07:02 +0100

    Initial official release

---
+++ precache-manifest.26e539abb136b92f9699b3177f3ade8d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4a940cddfa3ca076c2718c05a3cdc2",
+    "url": "/announcements.json"
+  },
+  {
+    "revision": "bb40e92a3f7326d9aaad",
+    "url": "/css/app.0fa6b8ea.css"
+  },
+  {
+    "revision": "a2daca502c4554859ff256829cbe238a",
+    "url": "/img/ex.png"
+  },
+  {
+    "revision": "b013a72767730c9cb853b0bcafa9a490",
+    "url": "/img/grid.png"
+  },
+  {
+    "revision": "e4eba04cb2c96adace07de9cfb7bc376",
+    "url": "/img/pmex.png"
+  },
+  {
+    "revision": "b66a32708813542e3664323fcd8e9afa",
+    "url": "/img/reddit.b66a3270.png"
+  },
+  {
+    "revision": "00c56a31afaa51419ec248100be97138",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb40e92a3f7326d9aaad",
+    "url": "/js/app.2ce42471.js"
+  },
+  {
+    "revision": "62245855cbf441b64d9c",
+    "url": "/js/chunk-vendors.5f495d89.js"
+  },
+  {
+    "revision": "c4986f076097ba47a52aad35ae2b0be9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e5a4392395768022683493ccb8cc1fcf",
+    "url": "/rumors.json"
+  }
+]);
