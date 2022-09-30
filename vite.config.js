10be1520b5f09646da3a0d9f34c9e23f40c0e6ef
Giovanni Santini <giovannisantini93@yahoo.it>
2022-09-30 14:48:56 +0200

    feat: Use Vite PWA plugin
    - Add the appropriate plugin and configure it correctly in Vite config
    - Remove old code, manually loading the PWA service worker, as the
      plugin does it itself
    - Small improvement to Vite config logic
    - Tweaks to index.html for the icons and theme color
    - Remove the now fully unused Vue CLI config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,60 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig(({ mode }) => {
-  const basePath = mode === "production" ? "/pmex-events/" : "/";
-  const useSourceMap = mode === "production";
+  const isDev = mode !== "production";
+  const basePath = isDev ? "/" : "/pmex-events/";
 
   return {
     base: basePath,
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      VitePWA({
+        includeAssets: [
+          "img/icons/favicon.ico",
+          "img/pmex.png",
+          "img/icons/apple-touch-icon.png",
+        ],
+        manifest: {
+          name: "Pokémon Masters EX Events",
+          short_name: "PMEX Events",
+          description:
+            "App tracking all the events happening in Pokémon Masters EX",
+          theme_color: "#f97f2c",
+          background_color: "#363434",
+          id: "io.github.itachisan.pmex_events",
+          icons: [
+            {
+              src: "./img/icons/android-chrome-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "./img/icons/android-chrome-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "./img/icons/android-chrome-maskable-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+              purpose: "maskable",
+            },
+            {
+              src: "./img/icons/android-chrome-maskable-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "maskable",
+            },
+          ],
+        },
+        workbox: {
+          cleanupOutdatedCaches: true,
+        },
+      }),
+    ],
     // Allow imports from '@'
     resolve: {
       alias: {
@@ -30,7 +75,7 @@ export default defineConfig(({ mode }) => {
     },
     /* Enable sourcemaps for easier debugging */
     build: {
-      sourcemap: useSourceMap,
+      sourcemap: isDev,
     },
   };
 });
